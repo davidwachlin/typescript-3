8f660546d552699a3e176b2ac3cfc19fa8846f43
David Wachlin <davidwachlin@gmail.com>
2019-12-25 15:30:31 -0500

    adds object type annotations

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -1,6 +1,6 @@
 let apples: number = 5;
 
-let speed: string = 'fast'
+let speed: string = 'fast';
 
 let hasName: boolean = true;
 
@@ -11,4 +11,19 @@ let nothing: undefined = undefined;
 // built in objects
 
 let now: Date = new Date();
+
+// Arrays
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, true, false];
+
+// Classes
+class Car {}
+let car: Car = new Car();
+
+// Object literal
+let point: { x: number; y: number } = {
+	x: 10,
+    y: 20
+};
 
