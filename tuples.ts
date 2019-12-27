76bf44a7d3a66e87c60682a9dc74886bea0b5472
David Wachlin <davidwachlin@gmail.com>
2019-12-27 16:16:15 -0500

    added type alias for tuple

---
+++ tuples.ts
@@ -0,0 +1,14 @@
+const drink = {
+    color: 'brown',
+    carbonated: true,
+    sugar: 40
+};
+
+// type alias
+type Drink = [string, boolean, number]
+
+// const pepsi: [string, boolean, number] = ['brown', true, 40];
+const pepsi: Drink = ['brown', true, 40];
+const sprite: Drink = ['brown', true, 40];
+const tea: Drink = ['brown', true, 40];
+
