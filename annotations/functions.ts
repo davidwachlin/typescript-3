e4b389ba31b5ead17c26c3ad66f175bb90dd9d54
David Wachlin <davidwachlin@gmail.com>
2019-12-27 14:54:22 -0500

    adds destructured paramaters with type annotations

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -13,3 +13,31 @@ function divide(a: number, b: number): number {
 const multiply = function(a: number, b: number): number {
     return a * b;
 }
+
+const logger = (message: string): void => {
+    console.log(message);
+}
+
+const throwError = (message: string): void => {
+     if (!message){
+         throw new Error(message);
+     }
+}
+
+const todaysWeather = {
+    date: new Date(),
+    weather: 'sunny'
+}
+
+const logWeather = ({ date, weather }: { date: Date, weather: string }): void => {
+    console.log(date)
+    console.log(weather)
+}
+
+// ES2015
+// const logWeather = ({ date, weather }) => {
+//     console.log(date)
+//     console.log(weather)
+// }
+
+logWeather(todaysWeather);
